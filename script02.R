0b53546c8d7ee38f6fae5ef3b06bea0013940463
Jgallo-R <jorgelgallos@gmail.com>
2020-06-06 19:23:18 -0500

    script02 : Primeros pasos con ggplot

--- script02.R
+++ script02.R
@@ -9,8 +9,8 @@ dir()
 #### cargar la data ####
 library(ggplot)
 library(ggplot2)
-library(help = car)
-library(help = carData)
+library(car)
+library(carData)
 data("Salaries")
 str(Salaries) # para ver su estuctura
 summary(Salaries)
@@ -48,9 +48,69 @@ help(ggsave)
 ggsave(filename = "Grafico3.png", width)
 
 # Grafico 4
-p + p + geom_point(aes(shape  = sex, col = sex), size = 3) + 
+p + geom_point(aes(shape  = sex, col = sex), size = 3) + 
   scale_shape_manual(values = c(4,21)) + 
-  theme(legend.position = c(0.95, 0.9))
+  scale_color_manual(values = c("red", "#5C7A54")) +
+  #theme(legend.position = "rigth") +
+  theme(legend.background = element_rect(fill = "#A2AFC9",
+                                         colour = "darkblue",
+                                         linetype = "dashed"))
+
+# modificar componentes de un theme
+#https://ggplot2.tidyverse.org/reference/theme.html
+
+
+# Grafico 5
+Graph5 <- p + geom_point(aes(shape  = sex, col = sex), size = 3) + 
+  scale_shape_manual(values = c(4,21)) + 
+  scale_color_manual(values = c("red", "#5C7A54")) +
+  #theme(legend.position = "rigth") +
+  theme(legend.background = element_rect(fill = "#A2AFC9")) +
+  scale_y_continuous(labels = scales::scientific)
+                                        
+# Grafico 6
+Graph5 + geom_smooth()
+ggsave(filename = "grafico6_GeomSmmoth.png")
+
+# Grafico 7
+Graph5 + geom_smooth(method = "lm", formula = y ~ poly(x,3))
+
+# Grafico 8
+Graph5 + geom_smooth(method = "lm", formula = y ~ poly(x,3)) +
+  facet_grid(~sex)# con esta grafica separamos por sub graficas por sexo
+
+# Grafico 9
+help("Salaries")
+
+p1 <- ggplot(data = Salaries, aes(x = yrs.service, y = salary , colour = rank))
+
+p1 + geom_point()
+
+p1 + geom_point()+
+  geom_smooth(aes(group = rank) , method = "lm", color = "black", size = 0.5)+
+  facet_grid(~sex)
+
+# Grafico 10
+p1 <- ggplot(data = Salaries, aes(x = yrs.service, y = salary , file = rank))
+p1 + geom_point(shape = 21) +
+  geom_smooth(method = "lm",
+              color = "black",
+              size = 0.5,
+              se = FALSE)+
+  facet_grid(~sex)
+
+library(gridExtra)
+
+# Grafico 11
+Plt1 <- ggplot(Salaries, aes(x = rank)) + geom_bar()
+Plt2 <- ggplot(Salaries, aes(x = sex)) + geom_bar()
+Plt3 <- ggplot(Salaries, aes(x = yrs.service, y = salary)) + geom_point()
+grid.arrange(Plt1,Plt2,Plt3, ncol = 3)
+
+
+
+
+
 
 
 
